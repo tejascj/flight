7feff208c8b96edee6b2ff95509b089f456b6c02
Tejas C J <77160762+tejascj@users.noreply.github.com>
2023-03-07 14:49:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,222 +1,259 @@
-// create a react component that input s a textarea message then perform a fetch request to localhost:3001 gets back a response as a data.message and displays that message in the box below
+import { useState } from 'react';
+import { Modal, Button, Form } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
 
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
-import { Form, ListGroup, Checkbox } from "react-bootstrap";
-import { airportsData } from "./airports.js";
-// import index from './index.js';
-
-
-export function Trip() {
-  const [isChecked, setIsChecked] = useState(false);
-
-  const [destination, setDestination] = useState('');
-  const [response, setResponse] = useState('');
-  const [days, setDays] = useState('');
-  // flight details
-  const [query1, setQuery1] = useState("");
-  const [selectedAirport1, setSelectedAirport1] = useState({ city: "", iata: "" });
-  const [query2, setQuery2] = useState("");
-  const [selectedAirport2, setSelectedAirport2] = useState({ city: "", iata: "" });
-
-  const filteredAirports1 = airportsData.filter((airport) =>
-    airport.city.toLowerCase().includes(query1.toLowerCase()) ||
-    airport.iata.toLowerCase().includes(query1.toLowerCase())
-  );
-
-  const filteredAirports2 = airportsData.filter((airport) =>
-    airport.city.toLowerCase().includes(query2.toLowerCase()) ||
-    airport.iata.toLowerCase().includes(query2.toLowerCase())
-  );
-
-  const handleInputChange1 = (event) => {
-    setQuery1(event.target.value);
-  };
-
-  const handleInputChange2 = (event) => {
-    setQuery2(event.target.value);
-  };
-
-  const handleOptionClick1 = (airport) => {
-    setSelectedAirport1(airport);
-    console.log(airport);
-    setQuery1(`${airport.city} (${airport.iata})`);
-  };
-
-  const handleOptionClick2 = (airport) => {
-    setSelectedAirport2(airport);
-    console.log(airport);
-    setQuery2(`${airport.city} (${airport.iata})`);
-  };
-
-
-  // handling the form submission for open ai and amadeus api
-  const [openAiResponse, setOpenAiResponse] = useState(null);
-  const [amadeusResponse, setAmadeusResponse] = useState(null);
-  const handleSubmit = (e) => {
+export function HomePage(props) {
+  const [showModal, setShowModal] = useState(false);
+  const handleGetStartedClick = () => setShowModal(true);
+  const handleModalClose = () => setShowModal(false);
+  //  handle login and create account
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const history = useHistory();
+  const [showLogin, setShowLogin] = useState(true);
+  // create account part
+  const handleCreateAccountClick = async (e) => {
     e.preventDefault();
-    fetch('http://localhost:3001/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        destination: destination,
-        days: days,
-        flight1: selectedAirport1.iata,
-        flight2: selectedAirport2.iata
-      })
-    })
-      .then(res => res.json())
-      .then(data => {
-        setOpenAiResponse(data,openAiResponse);
-        setAmadeusResponse(data,amadeusResponse);
-        console.log("OpenAI Response: ", data.openAiResponse);
-        console.log("Amadeus Response data: ", data.amadeusResponse);
+
+    try {
+      const response = await fetch('http://localhost:3001/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
       });
+
+      const data = await response.json();
+
+      if (data.status === 'success') {
+        // set the logged_in state to true and redirect to Trip component
+        setShowLogin(false);
+        props.setLoggedIn(true);
+        history.push('/trip');
+       
+      } else {
+        setError(data.message);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  // login part
+  const handleLoginClick = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:3001/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await response.json();
+
+      if (data.status === 'success') {
+        // set the logged_in state to true and redirect to Trip component
+        setShowLogin(true);
+        props.setLoggedIn(true);
+        history.push('/trip');
+        
+      } else {
+        setError(data.message);
+      }
+    } catch (error) {
+      console.error(error);
+    }
   };
 
 
-  return (
-    <div className="App">
 
-      <div class="container-fluid">
-        <nav class="navbar bg-body-tertiary">
-          <div class="container-fluid">
-            <a class="navbar-brand" mb-0 h1>Trip Planner AI</a>
-          </div>
-        </nav>
-      </div>
-      <div class="container border rounded p-3 ">
-        <form onSubmit={handleSubmit}>
+  const heroStyle = {
+    position: 'relative',
+    height: '100vh',
+    backgroundImage: 'url("https://mdbootstrap.com/img/new/fluid/city/018.jpg")',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  };
 
-          <div class="row g-3 align-items-center">
-            <div class="col-md-4">
-              <input type="text" class="form-control" value={destination} onChange={e => setDestination(e.target.value)} placeholder="Destination name" aria-label="Destination" />
-            </div>
-            <div class="col-md-4">
-              <input type="text" class="form-control" value={days} onChange={e => setDays(e.target.value)} placeholder="Number of Days" aria-label="No of days" />
-            </div>
-            <div class="col-md-2">
-              <input
-                className="form-check-input"
-                type="checkbox"
-                checked={isChecked}
-                onChange={() => setIsChecked(!isChecked)}
-              /><label className="form-check-label">
-                Cheapest Flight reccomendation
-              </label></div>
-            <div class="col-md-2 ">
-              <input type="submit" value="submit" class="form-control" aria-label="Last name" />
-            </div>
-            <div>
-              {isChecked && (
-                <>
-                  <Form.Control
-                    type="text"
-                    onChange={handleInputChange1}
-                    value={query1}
-                    placeholder="Search for an airport..."
-                  />
-                  {query1.length >= 3 && (
-                    <ListGroup style={{ position: "absolute", zIndex: 100 }}>
-                      {filteredAirports1.slice(0, 5).map((airport, index) => (
-                        <ListGroup.Item
-                          action
-                          key={index}
-                          onClick={() => handleOptionClick1(airport)}
-                        >
-                          {airport.city} ({airport.iata})
-                        </ListGroup.Item>
-                      ))}
-                    </ListGroup>
-                  )}
-                </>
-              )}
-            </div>
-            <div>
-              {isChecked && (
-                <>
-                  <Form.Control
-                    type="text"
-                    onChange={handleInputChange2}
-                    value={query2}
-                    placeholder="Search for an airport..."
-                  />
-                  {query2.length >= 3 && (
-                    <ListGroup style={{ position: "absolute", zIndex: 100 }}>
-                      {filteredAirports2.slice(0, 5).map((airport, index) => (
-                        <ListGroup.Item
-                          action
-                          key={index}
-                          onClick={() => handleOptionClick2(airport)}
-                        >
-                          {airport.city} ({airport.iata})
-                        </ListGroup.Item>
-                      ))}
-                    </ListGroup>
-                  )}
-                </>
-              )}
-            </div>
-          </div>
-        </form>
-      </div>
-      {openAiResponse ? (
-        <div class="container mx-auto px-auto w-50 bg-light">
-          <p>
-            {openAiResponse.split('\n').map((item, i) => {
-              return <React.Fragment key={i}>{item}<br /></React.Fragment>;
-            })}
-          </p>
-        </div>
-      ) : null}
-      {amadeusResponse ? (
-        <div>
-          <div class="container">
-            <div class="row">
-              <div class="col-4">
-                {/* <img src={`https://images.kiwi.com/airlines/64/${amadeusResponse.data.itineraries.segments.carrierCode}.png`} /> */}
-              </div>
-              <div class="col-5">
-                <div class="row">
-                  <div class="col-6">
-                    <div class="origin">
-                      {selectedAirport1.city}
-                      {/* <div class="departure-time">{amadeusResponse.data.itineraries.segments.departure.at}</div> */}
-                    </div>
-                  </div>
-                  <div class="col-6">
-                    <div class="destination">
-                      {selectedAirport2.city}
-                      {/* <div class="arrival-time">{amadeusResponse.data.itineraries.segments.arrival.at}</div> */}
-                    </div>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="col-12">
-                    {/* <div class="flight-code">Flight Code: {amadeusResponse.data.itineraries.segments.flightNumber}</div> */}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      ) : null}
-    </div>
-  );
-}
+  const overlayStyle = {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  };
 
-export function Flight() {
-  const [selectedAirport1, setSelectedAirport1] = useState(null);
-  const [selectedAirport2, setSelectedAirport2] = useState(null);
+  const contentStyle = {
+    textAlign: 'center',
+    color: '#fff',
+  };
+
+  const titleStyle = {
+    fontSize: '3rem',
+    fontWeight: 'bold',
+    marginBottom: '1rem',
+  };
+
+  const descriptionStyle = {
+    fontSize: '1.5rem',
+    marginBottom: '2rem',
+  };
+
+  const ctaButtonStyle = {
+    fontSize: '1.5rem',
+    padding: '1rem 2rem',
+    backgroundColor: 'transparent',
+    border: '2px solid #fff',
+    borderRadius: '30px',
+    color: '#fff',
+    transition: 'all 0.3s ease',
+    cursor: 'pointer',
+    outline: 'none',
+  };
+
+  const ctaButtonHoverStyle = {
+    backgroundColor: '#fff',
+    color: '#000',
+  };
+
+  const modalStyle = {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
+    display: showModal ? 'flex' : 'none',
+    alignItems: 'center',
+    justifyContent: 'center',
+    zIndex: 9999,
+  };
+
+  const formStyle = {
+    backgroundColor: '#fff',
+    padding: '3rem',
+    borderRadius: '10px',
+    boxShadow: '0 0 10px rgba(0, 0, 0, 0.3)',
+  };
+
+  const modalCloseButtonStyle = {
+    position: 'absolute',
+    top: '1rem',
+    right: '1rem',
+    fontSize: '1.5rem',
+    fontWeight: 'bold',
+    color: '#fff',
+    cursor: 'pointer',
+  };
+
   return (
     <div>
-      <div class="h1">
-        <h1>Flight</h1>
+      <nav
+        className="navbar navbar-expand-lg navbar-dark fixed-top"
+        style={{ backgroundColor: 'transparent' }}
+      >
+        <div className="container">
+          <a className="navbar-brand">
+            Trip Planner AI
+          </a>
+        </div>
+      </nav>
+      <div style={heroStyle}>
+        <div style={overlayStyle}>
+          <div style={contentStyle}>
+            <h1 style={titleStyle}>Plan Your Dream Trip with AI</h1>
+            <p style={descriptionStyle}>
+              Experience stress-free trip planning with AI.
+              Let our web application do the work for you, <br></br>finding the best deals on flights and create itinerary. Enjoy a personalized itinerary tailored just for you.
+            </p>
+            <a
+              className="btn btn-outline-light btn-lg m-2"
+              role="button"
+              rel="nofollow"
+              target="_blank"
+              onClick={handleGetStartedClick}
+            >
+              Get Started
+            </a>
+          </div>
+        </div>
       </div>
+      <Modal show={showModal} onHide={handleModalClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{showLogin ? "Login" : "Create Account"}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {showLogin ? (
+            <Form>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+              </Form.Group>
+
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+            </Form>
+          ) : (
+            <Form>
+              <Form.Group controlId="formBasicName">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="text" placeholder="Enter name" />
+              </Form.Group>
+
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+              </Form.Group>
+
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+
+              <Form.Group controlId="formBasicConfirmPassword">
+                <Form.Label>Confirm Password</Form.Label>
+                <Form.Control type="password" placeholder="Confirm Password" />
+              </Form.Group>
+            </Form>
+          )}
+        </Modal.Body>
+        <Modal.Footer>
+          {showLogin ? (
+            <>
+              <Button variant="link" onClick={handleCreateAccountClick}>
+                Create New Account
+              </Button>
+              <Button variant="primary" onClick={handleLoginClick}>
+                Login
+              </Button>
+
+            </>
+          ) : (
+            <>
+              <Button variant="link" onClick={handleLoginClick}>
+                Already have an account? Login
+              </Button>
+              <Button variant="primary" onClick={handleModalClose}>
+                Create Account
+              </Button>
+
+            </>
+          )}
+        </Modal.Footer>
+      </Modal>
+
     </div>
   );
 }
+
+
